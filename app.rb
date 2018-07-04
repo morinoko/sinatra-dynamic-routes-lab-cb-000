20ba3d7a1d4fd24146a80423281b6d8930f3a0c5
Felice Forby <feliceforby@gmail.com>
2018-07-04 22:00:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,14 +14,8 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @word1 = params[:word1]
-    @word2 = params[:word2]
-    @word3 = params[:word3]
-    @word4 = params[:word4]
-    @word5 = params[:word5]
-
-    params.values.each { |value| "#{value.to_s}" }
-    #{}"#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
+    @words = params.values
+    "#{@words.join(" ")}"
   end
 
   get '/:operation/:number1/:number2' do
