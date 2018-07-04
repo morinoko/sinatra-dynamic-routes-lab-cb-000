66e7dc413b6b14b552c87479484d6f58c0eedd27
Felice Forby <feliceforby@gmail.com>
2018-07-04 21:55:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
     @word5 = params[:word5]
 
     params.each { |k, v| "#{v}" }
-    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
+    #{}"#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
   end
 
   get '/:operation/:number1/:number2' do
