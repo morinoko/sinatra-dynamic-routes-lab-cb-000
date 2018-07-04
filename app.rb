0c0f887fb77f0a7bfb3e75d25ca64ede8952823b
Felice Forby <feliceforby@gmail.com>
2018-07-04 22:48:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     # number = params[:number].to_i
     # phrase = params[:phrase]
-    # 
+    #
     # number.times do
     #   "#{phrase}\n"
     # end
