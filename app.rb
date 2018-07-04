fb986ffbae04bf5f7f5368b064fb81ba85b7aa52
Felice Forby <feliceforby@gmail.com>
2018-07-04 21:57:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
     @word4 = params[:word4]
     @word5 = params[:word5]
 
-    params.each { |k, v| "#{v.to_s}" }
+    params.values.each { |value| "#{value.to_s}" }
     #{}"#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
   end
 
