dcd95702f3a3dd3fe7ae9e9c1f5b8df216c2e497
Felice Forby <feliceforby@gmail.com>
2018-07-04 21:44:38 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,14 +7,19 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
     
+    @number.times do
+      "#{phrase}\n"
+    end
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    
+
   end
 
   get '/:operation/:number1/:number2' do
-    
+
   end
 end
