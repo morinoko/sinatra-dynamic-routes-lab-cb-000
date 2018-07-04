b0f0d8979718a1a7546f795b71deab3e5c2006a0
Felice Forby <feliceforby@gmail.com>
2018-07-04 22:25:38 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -31,6 +31,12 @@ class App < Sinatra::Base
     case @operation
     when "add"
       "#{@num1 + @num2}"
+    when "subtract"
+      "#{@num1 - @num2}"
+    when "multiply"
+      "#{@num1 * @num2}"
+    when "divide"
+      "#{@num1 / @num2}"
     else
       "Not a valid operation"
     end
