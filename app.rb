f4dcda75f1d096fde59788be72a8e55439ae9a5e
Felice Forby <feliceforby@gmail.com>
2018-07-04 22:45:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ class App < Sinatra::Base
   end
 
   get '/square/:number' do
-    @number = params[:number].to_i
-    "#{@number * @number}"
+    number = params[:number].to_i
+    "#{number**2}"
   end
 
   get '/say/:number/:phrase' do
